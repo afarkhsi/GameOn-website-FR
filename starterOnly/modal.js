9aba15f7776f926528448d9d15815bfc8b634639
abdou fk <abdourrhamanefarkhsi@gmail.com>
2023-02-28 18:21:01 +0100

    Fermeture modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,17 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+  // Ajouts
+const closeBtn = document.querySelectorAll(".close");
+let form = document.querySelector(".form")
+// const submit = document.querySelector(".btn-submit");
+// const prenom = document.querySelector("#prenom");
+const nom = document.querySelector("#nom");
+const mail = document.querySelector("#mail");
+const nbTournois = document.querySelector("#nb-tournois");
+const tournoiSelection = document.querySelector(".tournoi-selection");
+const checkboxObligatory = document.querySelector("#checkbox1");
+const checkboxOptional = document.querySelector("#checkbox2");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +30,14 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// close modal event
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+
 
 
